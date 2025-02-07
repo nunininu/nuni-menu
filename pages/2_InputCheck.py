653f24e1caa82ce67ea0df3e1add7024f66109c0
nunininu <sgcho0907@gmail.com>
2025-02-07 17:40:50 +0900

    ing ...

--- pages/2_InputCheck.py
+++ pages/2_InputCheck.py
@@ -1,13 +1,13 @@
 import streamlit as st
-from nuni_menu.db import select_empty_table
+from nuni_menu.db import select_members_without_lunch
 
 st.subheader("입력 안 한 사람 잡아내기")
 checkPress = st.button ("입력 안 한 사람은?")
 
 if checkPress:
     try:
-        df = select_empty_table()
-        df
+        df = select_members_without_lunch()
+        st.dataframe(df, use_container_width=True)
     except Exception as e:
         st.warning(f"조회 중 오류가 발생했습니다")
         print(f"Exception: {e}")
